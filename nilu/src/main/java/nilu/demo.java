b4126c44bf9bb8dda604eacb575135c41b1f9443
manojsonavane2810 <118589311+manojsonavane2810@users.noreply.github.com>
2022-11-19 11:48:04 +0530

    Update demo.java

--- nilu/src/main/java/nilu/demo.java
+++ nilu/src/main/java/nilu/demo.java
@@ -5,7 +5,7 @@ public class demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("tharki SALA nikhil");
-		
+	System.out.println("BEHANON KA BHAIYA MOTGARE");	
 	}
 
 }
